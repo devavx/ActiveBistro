21c108bbaaf087f72ab8f7d66c12468f17439187
Aviral Singh <dev.aviralx@gmail.com>
2020-08-26 18:35:09 +0530

    Updates

--- app/Core/Enums/Common/Directories.php
+++ app/Core/Enums/Common/Directories.php
@@ -11,4 +11,5 @@ class Directories extends Enum
     const MealPlanImages = "meal_plan_images";
     const ItemImages = "item_images";
     const UserProfileImages = "user_profile_images";
+    const HomeSliderImages = "home_slider_images";
 }
--- app/HomeSetting.php
+++ app/HomeSetting.php
@@ -2,14 +2,25 @@
 
 namespace App;
 
+use App\Core\Enums\Common\Directories;
 use App\Core\Facades\Uploads;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\UploadedFile;
 
 class HomeSetting extends Model
 {
     protected $fillable = ['title', 'description', 'thumbnail', 'type', 'other_option', 'active'];
 
-    public function getThumbnailAttribute()
+    public function setThumbnailAttribute($value): void
+    {
+        if ($value instanceof UploadedFile) {
+            $this->attributes['thumbnail'] = Uploads::instance()->putFile(Directories::HomeSliderImages, $value);
+        } else {
+            $this->attributes['thumbnail'] = $value;
+        }
+    }
+
+    public function getThumbnailAttribute($value): ?string
     {
         return Uploads::existsUrl($this->attributes['thumbnail']);
     }
