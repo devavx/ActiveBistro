3f017d16421e5b7d8851dc3ed09bae8b9720c158
Aviral Singh <dev.aviralx@gmail.com>
2020-09-14 19:31:50 +0530

    Updates

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -24,25 +24,35 @@ class PaymentController extends Controller
 		$payload['items'] = $state->items();
 		$payload['invoice_id'] = $state->invoiceId();
 		$payload['invoice_description'] = "Order_{$payload['invoice_id']}";
-		$payload['return_url'] = route('payments.success');
+		$payload['return_url'] = route('payments.completed');
 		$payload['cancel_url'] = route('payments.cancelled');
 		$payload['total'] = $state->total();
 		$response = $this->provider->setExpressCheckout($payload);
 		return redirect()->to($response['paypal_link']);
 	}
 
-	public function success ()
+	public function completed ()
 	{
 		$response = $this->provider->getExpressCheckoutDetails(request('token'));
 		if (in_array(strtoupper($response['ACK']), ['SUCCESS', 'SUCCESSWITHWARNING'])) {
-			return view('frontend.checkout_success');
+			return redirect()->route('payments.completed')->header('Cache-Control', 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
 		} else {
-			return view('frontend.checkout_failed');
+			return redirect()->route('payments.cancelled')->header('Cache-Control', 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
 		}
 	}
 
 	public function cancelled ()
 	{
-		return redirect()->route('cart.index')->with('errormsg', 'Your transaction was cancelled successfully!');
+		return redirect()->route('cart.index')->with('success', 'Your transaction was cancelled successfully!');
+	}
+
+	public function confirmed ()
+	{
+		return view('frontend.checkout_success');
+	}
+
+	public function failed ()
+	{
+		return view('frontend.checkout_failed');
 	}
 }
