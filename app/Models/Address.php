f14187e1a615303f454c6a1591a343058bfde24a
Aviral Singh <dev.aviralx@gmail.com>
2020-09-14 20:33:48 +0530

    Updates

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -6,12 +6,14 @@ use App\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Address extends Model {
-    protected $fillable = [
-        'user_id', 'day', 'address_first_line', 'address_second_line', 'city', 'postcode', 'delivery_notes'
-    ];
+class Address extends Model
+{
+	protected $fillable = [
+		'user_id', 'day', 'address_first_line', 'address_second_line', 'city', 'postcode', 'delivery_notes'
+	];
 
-    public function user (): BelongsTo {
-        return $this->belongsTo(User::class);
-    }
+	public function user (): BelongsTo
+	{
+		return $this->belongsTo(User::class);
+	}
 }
