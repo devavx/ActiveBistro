f2ecb0af9a52be70955956b76df588244c59121a
Aviral Singh <dev.aviralx@gmail.com>
2020-09-18 14:17:44 +0530

    Updates

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -8,9 +8,9 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-12">
-					@if($coupon!=null&&$coupon->isValid())
+					@if($coupon!=null)
 						<h6 class="text-white m-0 text-center">Seen us on
-							<a href="#" class="font-weight-bold text-white" data-toggle="tooltip" title="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.">Click Here to get 50% OFF your first 2 boxes.</a>
+							<a href="#" class="font-weight-bold text-white" data-toggle="tooltip" title="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.">{{$coupon->description}}</a>
 						</h6>
 					@endif
 				</div>
