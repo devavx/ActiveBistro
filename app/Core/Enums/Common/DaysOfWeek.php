f89f4732c7adaf0b3122a9456dfe6a46e19a47b3
Aviral Singh <dev.aviralx@gmail.com>
2020-10-06 20:35:18 +0530

    Updates

--- app/Core/Enums/Common/DaysOfWeek.php
+++ app/Core/Enums/Common/DaysOfWeek.php
@@ -1,29 +1,35 @@
 <?php
 
-
 namespace App\Core\Enums\Common;
-
 
 use BenSampo\Enum\Enum;
 
 class DaysOfWeek extends Enum
 {
-    const Monday = 'monday';
-	const Sunday = 'sunday';
+	const Monday = 'monday';
 	const Tuesday = 'tuesday';
-    const Wednesday = 'wednesday';
-    const Thursday = 'thursday';
-    const Friday = 'friday';
-    const Saturday = 'saturday';
+	const Wednesday = 'wednesday';
+	const Thursday = 'thursday';
+	const Friday = 'friday';
+	const Saturday = 'saturday';
+	const Sunday = 'sunday';
 
-    /**
-     * You can manipulate this sequence to control the order
-     * in which days and their respective meals are listed.
-     * Default order is the sequence in which the const values are defined.
-     * @return array
-     */
-    public static function sequence(): array
-    {
-        return self::getValues();
-    }
+	/**
+	 * You can manipulate this sequence to control the order
+	 * in which days and their respective meals are listed.
+	 * Default order is the sequence in which the const values are defined.
+	 * @return array
+	 */
+	public static function sequence (): array
+	{
+		return [
+			self::Monday,
+			self::Tuesday,
+			self::Wednesday,
+			self::Thursday,
+			self::Friday,
+			self::Saturday,
+			self::Sunday
+		];
+	}
 }
