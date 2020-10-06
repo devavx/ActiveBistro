2d04748ff348692737d1e3f5a16b59717b031c4c
Aviral Singh <dev.aviralx@gmail.com>
2020-10-06 17:10:50 +0530

    Updates

--- app/Core/Enums/Common/DietaryRequirement.php
+++ app/Core/Enums/Common/DietaryRequirement.php
@@ -1,14 +1,24 @@
 <?php
 
-
 namespace App\Core\Enums\Common;
-
 
 use BenSampo\Enum\Enum;
 
-class DietaryRequirement extends Enum {
-    const None = "none";
-    const GlutenFree = "gluten_free";
-    const Vegetarian = "vegetarian";
-    const Vegan = "vegan";
+class DietaryRequirement extends Enum
+{
+	const None = "none";
+	const GlutenFree = "gluten_free";
+	const Vegetarian = "vegetarian";
+	const Vegan = "vegan";
+	protected const Names = [
+		self::None => "None",
+		self::GlutenFree => "Gluten Free",
+		self::Vegetarian => "Vegetarian",
+		self::Vegan => "Vegan",
+	];
+
+	public static function displayName (string $key): ?string
+	{
+		return self::Names[$key];
+	}
 }
