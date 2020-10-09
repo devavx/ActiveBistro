c70727b6a697b004ddc91e7f8a782f00b4c0b279
Aviral Singh <dev.aviralx@gmail.com>
2020-10-09 14:32:15 +0530

    Updates

--- resources/views/frontend/coupon_frame.blade.php
+++ resources/views/frontend/coupon_frame.blade.php
@@ -1,7 +1,7 @@
 @if($coupon!=null)
 	<span id="couponText">{{$coupon->code}}</span>
-	<a class="text-color" href="removeCoupon();" id="removeLink"> Click here.</a>
+	<a class="text-color" href="javascript:removeCoupon();" id="removeLink"> Remove</a>
 @else
 	<span id="couponText">Referred by a friend or got a coupon?</span>
-	<a class="text-color" data-toggle="modal" data-target="#couponModal" href="javascript:void(0);" id="applyLink"> Click here.</a>
+	<a class="text-color" data-toggle="modal" data-target="#couponModal" href="javascript:void(0);" id="applyLink"> Click here</a>
 @endif
