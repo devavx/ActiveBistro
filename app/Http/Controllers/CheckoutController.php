9b389cdbae8d522b5756a7285b74bcf63078c4be
Aviral Singh <dev.aviralx@gmail.com>
2020-10-12 18:03:44 +0530

    Changes in checkout UI & flow

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -131,7 +131,7 @@ class CheckoutController extends Controller
 		$payload['invoice_description'] = $state->invoice()->description;
 		$payload['return_url'] = route('payments.completed');
 		$payload['cancel_url'] = route('payments.cancelled');
-		$payload['total'] = $state->total();
+		$payload['amount'] = $state->total();
 		$response = $this->provider->setExpressCheckout($payload);
 		dd($response);
 		return redirect()->to($response['paypal_link']);
