12b3d84f4187f060abd4f55641069eb413dc562e
Aviral Singh <dev.aviralx@gmail.com>
2020-10-12 13:47:48 +0530

    Checking paypal redirect issue

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -132,6 +132,7 @@ class CheckoutController extends Controller
 		$payload['return_url'] = route('payments.completed');
 		$payload['cancel_url'] = route('payments.cancelled');
 		$payload['total'] = $state->total();
+		dd($payload);
 		$response = $this->provider->setExpressCheckout($payload);
 		dd($response);
 		return redirect()->to($response['paypal_link']);
